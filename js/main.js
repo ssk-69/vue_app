bde916511b75534c9b8d9353288714166d1f1acb
hinata-bokko <chokichoki4869@gmail.com>
2021-09-29 23:58:17 +0900

    master

---
+++ js/main.js
@@ -0,0 +1,50 @@
+(function(){
+  'use scrict';
+
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true 
+      }]
+    },
+    methods: {
+      addItem: function() {
+        var　item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?'))
+        {
+        this.todos.splice(index, 1);
+        }
+      },
+      purge: function() {
+        if (!confirm('delete finished?')) {
+            return;
+          }
+          this.todos = this.remaining;
+        }
+    },
+    computed: {
+      remaining: function() {
+       return this.todos.filter(function(todo) {
+         return !todo.isDone;
+       });
+      }
+    }
+  });
+})();
